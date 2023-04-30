8a3274938e6b88e1515c6ce56ddc424bfe74086f
Richard Torenvliet <richard@torenvliet.org>
2023-04-30 16:52:02 +0200

    Add unit test for resolver

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ## Introduction
 
-This is a nestJS application illustration the use of CQRS in combination with Event Sourcing. For this project
-I have chosen to use an in-memory EventStore. Meaning that the events are not persisted to a database. This is
-done to keep the project simple and to focus on the CQRS and Event Sourcing part of the application.
+This is a **NestJS** application that illustrates the use of **CQRS** in combination with **Event Sourcing**. For this project
+I have chosen to use an in-memory **EventStore**, by building my own simple EventStore. This means that the events are
+not persisted to a database. This is done to keep the project simple and to focus on the CQRS and Event Sourcing part of
+the application.
 
-I've written tests for the CommandsHandlers, EventsHandlers and QueryHandlers. Most intresting parts of the code
+I've written tests for the **CommandHandlers**, **EventHandlers** and **QueryHandlers**. Most interesting parts of the code
 live in the `products` module. The `products` module is a simple CRUD module that uses CQRS and Event Sourcing.
 
 The in-memory EventStore is implemented is located top-level in the `src/event-store.service.ts` file. The EventStore
---
+++ src/products/product.resolver.spec.ts
@@ -0,0 +1,108 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CommandBus, EventBus, EventPublisher, QueryBus } from '@nestjs/cqrs';
+
+import { ProductsResolver } from './products.resolver';
+import { ProductsService } from './products.service';
+import { FindByIdProductQuery } from './queries/impl/find-by-id-product.query';
+import { ProductQueryHandlers } from './queries/handlers';
+import { ProductCommandHandlers } from './commands/handlers';
+import { ProductsRepository } from './products.repository';
+import { EventStoreService } from '../event-store.service';
+import { EventHandlers } from './products.module';
+import { FindByIdProductsHandler } from './queries/handlers/find-by-id-products.handler';
+import { FindAllProductsQuery } from './queries/impl/find-all-products.query';
+import { FindAllProductsHandler } from './queries/handlers/find-all-products.handler';
+import { CreateProductCommand } from './commands/impl/create-product.command';
+import { CreateProductHandler } from './commands/handlers/create-product.handler';
+import { CreateProductInputWithId } from './dto/create-product.input';
+import { RemoveProductCommand } from './commands/impl/remove-product.command';
+import { RemoveProductHandler } from './commands/handlers/remove-product.handler';
+
+describe('ProductCommands', function () {
+  let queryBus: QueryBus;
+  let resolver: ProductsResolver;
+  let service: ProductsService;
+  let commandBus: CommandBus;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        ...EventHandlers,
+        ...ProductCommandHandlers,
+        ...ProductQueryHandlers,
+        ProductsResolver,
+        ProductsService,
+        CommandBus,
+        QueryBus,
+        EventBus,
+        EventPublisher,
+        ProductsRepository,
+        EventStoreService,
+      ],
+    }).compile();
+
+    resolver = module.get<ProductsResolver>(ProductsResolver);
+    service = module.get<ProductsService>(ProductsService);
+
+    commandBus = module.get<CommandBus>(CommandBus);
+    queryBus = module.get<QueryBus>(QueryBus);
+  });
+
+  describe('GetProductById', () => {
+    it('It retrieve a product by Id - call the right queryBus', async () => {
+      const spy = jest.spyOn(queryBus, 'execute').mockImplementation();
+
+      queryBus.register([FindByIdProductsHandler]);
+
+      await resolver.findOne('1');
+
+      expect(spy).toHaveBeenCalledWith(new FindByIdProductQuery('1'));
+    });
+  });
+
+  describe('GetProducts', () => {
+    it('It retrieve all products - call the right queryBus', async () => {
+      const spy = jest.spyOn(queryBus, 'execute').mockImplementation();
+
+      queryBus.register([FindAllProductsHandler]);
+
+      await resolver.findAll();
+
+      expect(spy).toHaveBeenCalledWith(new FindAllProductsQuery());
+    });
+  });
+
+  describe('Create a product', () => {
+    it('Create a new product - call the right command bus', async () => {
+      const product: CreateProductInputWithId = {
+        id: Math.random().toString(),
+        name: 'Product 1',
+        price: 9.99,
+        description: 'Product description',
+        sku: 'product-1',
+      };
+
+      const spy = jest.spyOn(commandBus, 'execute').mockImplementation();
+
+      commandBus.register([CreateProductHandler]);
+
+      await resolver.createProduct(product);
+
+      expect(spy).toHaveBeenCalledWith(new CreateProductCommand(product));
+    });
+  });
+
+  describe('Remove a product', () => {
+    it('Remove a product - call the right command bus', async () => {
+      const productId = Math.random().toString();
+
+      const spy = jest.spyOn(commandBus, 'execute').mockImplementation();
+
+      commandBus.register([RemoveProductHandler]);
+
+      await resolver.removeProduct(productId);
+
+      expect(spy).toHaveBeenCalledWith(new RemoveProductCommand(productId));
+    });
+  });
+});
